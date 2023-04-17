8df04158068d664ad9b1c3dab774d1dba02b78f7
AnnaK <anna_morozova_kz@list.ru>
2023-04-17 18:29:41 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для паспорт
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public Boolean subscription; // поле для согласия на смс
+    public FormDate birthday; // дата рождения
+
+    // добавьте другие поля
+}
